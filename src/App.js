9554de01e5035001d6a7b3d253d3487c99a898ca
suvansh mahajan <smahajan@demystdata.com>
2024-07-10 00:33:59 +0530

    create traffic-light project with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style.css';
+import { useState, useEffect } from 'react';
 function App() {
+ const [currentLight, setCurrentLight] = useState('green');
+ useEffect(() => {
+  let timer;
+  if (currentLight === 'green') {
+    timer = setTimeout(() => setCurrentLight('yellow'), 4000);
+  } else if (currentLight === 'yellow') {
+    timer = setTimeout(() => setCurrentLight('red'), 1000);
+  } else if (currentLight === 'red') {
+    timer = setTimeout(() => setCurrentLight('green'), 3000);
+  }
+
+  return () => clearTimeout(timer);
+},[currentLight]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="traffic-light">
+        <div className="light red" role='red-light' style={{backgroundColor: currentLight ==='red' ? 'red' :'black'}}></div>
+        <div className="light yellow" role='yellow-light' style={{backgroundColor:currentLight ==='yellow' ? 'yellow' :'black'}}></div>
+        <div className="light green" role='green-light' style={{backgroundColor:currentLight ==='green' ? 'green' :'black'}}></div>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ src/unit_test.test.js
@@ -0,0 +1,30 @@
+// src/App.test.js
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+jest.setTimeout(10000);
+test('renders traffic light', () => {
+  render(<App />);
+  const redLight = screen.getByRole('red-light');
+  const yellowLight = screen.getByRole('yellow-light');
+  const greenLight = screen.getByRole('green-light');
+  expect(redLight).toBeInTheDocument();
+  expect(yellowLight).toBeInTheDocument();
+  expect(greenLight).toBeInTheDocument();
+});
+
+test('traffic light cycles through colors', async () => {
+    
+  render(<App />);
+  const redLight = screen.getByRole('red-light');
+  const yellowLight = screen.getByRole('yellow-light');
+  const greenLight = screen.getByRole('green-light');
+
+  expect(greenLight).toHaveStyle('background-color: green');
+  
+  await waitFor(() => expect(yellowLight).toHaveStyle('background-color: yellow'), { timeout: 4000 });
+  await waitFor(() => expect(redLight).toHaveStyle('background-color: red'), { timeout: 5000 });
+  await waitFor(() => expect(greenLight).toHaveStyle('background-color: green'), { timeout: 8000 });
+});
