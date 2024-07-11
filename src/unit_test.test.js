bb9c37aa2d9f25e16d95960245361f8129d10b54
suvansh mahajan <smahajan@demystdata.com>
2024-07-12 01:32:04 +0530

    use fakeTimers to quickly execute unit tests

--- src/unit_test.test.js
+++ src/unit_test.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 jest.setTimeout(10000);
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.useRealTimers();
+});
+
 test('renders traffic light', () => {
   render(<App />);
   const redLight = screen.getByRole('red-light');
@@ -15,15 +24,25 @@ test('renders traffic light', () => {
 });
 
 test('traffic light cycles through colors', async () => {
-    
   render(<App />);
   const redLight = screen.getByRole('red-light');
   const yellowLight = screen.getByRole('yellow-light');
   const greenLight = screen.getByRole('green-light');
 
   expect(greenLight).toHaveStyle('background-color: green');
-  
-  await waitFor(() => expect(yellowLight).toHaveStyle('background-color: yellow'), { timeout: 4000 });
-  await waitFor(() => expect(redLight).toHaveStyle('background-color: red'), { timeout: 5000 });
-  await waitFor(() => expect(greenLight).toHaveStyle('background-color: green'), { timeout: 8000 });
+
+  act(() => {
+    jest.advanceTimersByTime(4000);
+  });
+  await waitFor(() => expect(yellowLight).toHaveStyle('background-color: yellow'));
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+  await waitFor(() => expect(redLight).toHaveStyle('background-color: red'));
+
+  act(() => {
+    jest.advanceTimersByTime(3000);
+  });
+  await waitFor(() => expect(greenLight).toHaveStyle('background-color: green'));
 });
