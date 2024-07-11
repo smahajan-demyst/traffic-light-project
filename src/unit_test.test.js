8b5a2cb883e4335e43639a3e30a3122f8234818f
suvansh mahajan <smahajan@demystdata.com>
2024-07-12 01:09:16 +0530

    modify dependencies

--- src/unit_test.test.js
+++ src/unit_test.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import App from './App';
 
 jest.setTimeout(10000);
